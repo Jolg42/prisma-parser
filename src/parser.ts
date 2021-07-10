ec7ed8b5450425f51edf1c4fd3fe4f4aeb1ce56e
Sergey Tatarintsev <sergey@tatarintsev.me>
2021-07-10 16:20:59 +0200

    Lazily compute parsing functions
    1. Allows to easily define recursive rules
    2. Allows to write whole parser code in top-down manner as opposed to
       bottom up. Subjectively, it is easier to read parsing code when
       starting from the top.
    3. Might improve performance a little, since the rule will now never be
       created if there was no need for it during parsing

--- src/parser.ts
+++ src/parser.ts
@@ -3,14 +3,14 @@ import { Token, TokenList, TokenType } from './tokenizer'
 import { Node, DocumentNode, TypeModifier, Position, ConfigType } from './ast'
 import { PrismaParsingError } from './prisma-parsing-error'
 
-export function parse(source: string): DocumentNode {
-  return document(new ParserState(source)).unwrap()
-}
-
 type ParsingResult<T> = Result<T, PrismaParsingError>
 type RuleFunction<ReturnType> = (state: ParserState) => ParsingResult<ReturnType>
 
 type TupleToUnion<TupleType extends unknown[]> = TupleType[number]
+
+export function parse(source: string): DocumentNode {
+  return document(new ParserState(source)).unwrap()
+}
 
 class ParserState {
   private tokenList: TokenList
@@ -42,30 +42,58 @@ class ParserState {
   }
 }
 
-const attribute = defineNodeRule(token('attribute'), (token) => ({
-  kind: 'Attribute',
-  name: token.token,
-  location: {
-    start: token.start,
-    end: token.end,
-  },
-}))
+const document = defineNodeRule(
+  () => takeUntil(definition, 'eof'),
+  ([definitions, eof]) => ({
+    kind: 'Document',
+    definitions,
+    location: {
+      start: definitions[0]?.location.start ?? 0,
+      end: eof.end,
+    },
+  }),
+)
 
-const identifier = defineNodeRule(token('identifier'), (token) => ({
-  kind: 'Identifier',
-  identifier: token.token,
-  location: {
-    start: token.start,
-    end: token.end,
-  },
-}))
+const definition = defineRule(() => choice('Expected config or model definition', modelDefinition, configDefinition))
 
-const typeModifierToken = defineRule<Token | null>((state) => {
-  return (optionalToken(state) as ParsingResult<Token | null>).orElse(() => arrayToken(state)).orElse(() => ok(null))
-})
+const modelDefinition = defineNodeRule(
+  () =>
+    sequence(
+      keyword('model'),
+      changeErrorMessage(identifier, 'Expected model name'),
+      token('{'),
+      takeUntil(fieldDefinition, '}'),
+    ),
+  ([modelKeyword, name, , [fields, closeBrace]]) => ({
+    kind: 'ModelDefinition',
+    name,
+    fields,
+    location: {
+      start: modelKeyword.start,
+      end: closeBrace.end,
+    },
+  }),
+)
+
+const fieldDefinition = defineNodeRule(
+  () => sequence(changeErrorMessage(identifier, 'Expected field name'), type, zeroOrMore(attribute)),
+  ([name, type, attributes]) => {
+    const end = attributes.length > 0 ? attributes[attributes.length - 1].location.end : type.location.end
+    return {
+      kind: 'FieldDefinition',
+      name,
+      type,
+      attributes,
+      location: {
+        start: name.location.start,
+        end,
+      },
+    }
+  },
+)
 
 const type = defineNodeRule(
-  sequence(changeErrorMessage(identifier, 'Expected field type'), typeModifierToken),
+  () => sequence(changeErrorMessage(identifier, 'Expected field type'), typeModifierToken),
   ([name, modifierToken]) => {
     let modifier: TypeModifier = 'none'
     let end = name.location.end
@@ -86,82 +114,24 @@ const type = defineNodeRule(
   },
 )
 
-const fieldDefinition = defineNodeRule(
-  sequence(changeErrorMessage(identifier, 'Expected field name'), type, zeroOrMore(attribute)),
-  ([name, type, attributes]) => {
-    const end = attributes.length > 0 ? attributes[attributes.length - 1].location.end : type.location.end
-    return {
-      kind: 'FieldDefinition',
-      name,
-      type,
-      attributes,
-      location: {
-        start: name.location.start,
-        end,
-      },
-    }
-  },
-)
+const typeModifierToken = defineRule<Token | null>(() => (state) => {
+  return (optionalToken(state) as ParsingResult<Token | null>).orElse(() => arrayToken(state)).orElse(() => ok(null))
+})
 
-const modelDefinition = defineNodeRule(
-  sequence(
-    keyword('model'),
-    changeErrorMessage(identifier, 'Expected model name'),
-    token('{'),
-    takeUntil(fieldDefinition, '}'),
-  ),
-  ([modelKeyword, name, , [fields, closeBrace]]) => ({
-    kind: 'ModelDefinition',
-    name,
-    fields,
-    location: {
-      start: modelKeyword.start,
-      end: closeBrace.end,
-    },
-  }),
-)
-
-const stringLiteral = defineNodeRule(token('string'), (token) => {
-  const value = token.token.replace(/^"/, '').replace(/"$/, '').replace(/\\"/g, '"')
-  return {
-    kind: 'StringLiteral',
-    value,
+const attribute = defineNodeRule(
+  () => token('attribute'),
+  (token) => ({
+    kind: 'Attribute',
+    name: token.token,
     location: {
       start: token.start,
       end: token.end,
     },
-  }
-})
-
-const booleanLiteral = defineNodeRule(
-  choice('Expected boolean value', keyword('true'), keyword('false')),
-  (keywordToken) => ({
-    kind: 'BooleanLiteral',
-    value: keywordToken.token === 'true' ? true : false,
-
-    location: {
-      start: keywordToken.start,
-      end: keywordToken.end,
-    },
   }),
 )
 
-const expression = choice('Expected expression', stringLiteral, booleanLiteral)
-
-const configOption = defineNodeRule(sequence(identifier, token('='), expression), ([key, , value]) => ({
-  kind: 'ConfigOption',
-  key,
-  value,
-  location: {
-    start: key.location.start,
-    end: value.location.end,
-  },
-}))
-
-const configType = choice('Expected generator or datasource keyword', keyword('datasource'), keyword('generator'))
-
 const configDefinition = defineNodeRule(
-  sequence(configType, identifier, token('{'), takeUntil(configOption, '}')),
+  () => sequence(configType, identifier, token('{'), takeUntil(configOption, '}')),
   ([configTypeToken, name, , [options, closingBrace]]) => ({
     kind: 'ConfigDefinition',
     type: configTypeToken.token as ConfigType,
@@ -174,20 +144,69 @@ const configDefinition = defineNodeRule(
   }),
 )
 
-const definition = choice('Expected config or model definition', modelDefinition, configDefinition)
-const document = defineNodeRule(takeUntil(definition, 'eof'), ([definitions, eof]) => ({
-  kind: 'Document',
-  definitions,
-  location: {
-    start: definitions[0]?.location.start ?? 0,
-    end: eof.end,
+const configType = defineRule(() =>
+  choice('Expected generator or datasource keyword', keyword('datasource'), keyword('generator')),
+)
+
+const configOption = defineNodeRule(
+  () => sequence(identifier, token('='), expression),
+  ([key, , value]) => ({
+    kind: 'ConfigOption',
+    key,
+    value,
+    location: {
+      start: key.location.start,
+      end: value.location.end,
+    },
+  }),
+)
+
+const expression = defineRule(() => choice('Expected expression', stringLiteral, booleanLiteral))
+
+const stringLiteral = defineNodeRule(
+  () => token('string'),
+  (token) => {
+    const value = token.token.replace(/^"/, '').replace(/"$/, '').replace(/\\"/g, '"')
+    return {
+      kind: 'StringLiteral',
+      value,
+      location: {
+        start: token.start,
+        end: token.end,
+      },
+    }
   },
-}))
+)
+
+const booleanLiteral = defineNodeRule(
+  () => choice('Expected boolean value', keyword('true'), keyword('false')),
+  (keywordToken) => ({
+    kind: 'BooleanLiteral',
+    value: keywordToken.token === 'true' ? true : false,
+
+    location: {
+      start: keywordToken.start,
+      end: keywordToken.end,
+    },
+  }),
+)
+
+const identifier = defineNodeRule(
+  () => token('identifier'),
+  (token) => ({
+    kind: 'Identifier',
+    identifier: token.token,
+    location: {
+      start: token.start,
+      end: token.end,
+    },
+  }),
+)
 
 function sequence<Elements extends unknown[]>(
   ...ruleFunctions: { [K in keyof Elements]: RuleFunction<Elements[K]> }
 ): RuleFunction<Elements> {
-  return defineRule((state) => {
+  return defineRule(() => (state) => {
     let result = ok([] as unknown[])
     for (const rule of ruleFunctions) {
       result = result.flatMap((elements) => {
@@ -203,7 +222,7 @@ function choice<Elements extends unknown[]>(
   defaultError: string,
   ...ruleFunctions: { [K in keyof Elements]: RuleFunction<Elements[K]> }
 ): RuleFunction<TupleToUnion<Elements>> {
-  return defineRule((state) => {
+  return defineRule(() => (state) => {
     let result = state.error(defaultError, state.peek().start)
     for (const rule of ruleFunctions) {
       result = result.orElse((previousError) => {
@@ -225,7 +244,7 @@ function takeUntil<ReturnType>(
   elementRule: RuleFunction<ReturnType>,
   tokenType: TokenType,
 ): RuleFunction<[ReturnType[], Token]> {
-  return (state) => {
+  return defineRule(() => (state) => {
     let result: ParsingResult<ReturnType[]> = ok([])
     while (state.peek().type !== tokenType && !result.isError()) {
       result = result.flatMap((elements) => {
@@ -233,11 +252,11 @@ function takeUntil<ReturnType>(
       })
     }
     return result.map((values) => [values, state.advance()])
-  }
+  })
 }
 
 function zeroOrMore<ReturnType>(elementRule: RuleFunction<ReturnType>): RuleFunction<ReturnType[]> {
-  return (state) => {
+  return defineRule(() => (state) => {
     const result: ReturnType[] = []
     let ruleResult = elementRule(state)
     while (ruleResult.isOk()) {
@@ -245,15 +264,15 @@ function zeroOrMore<ReturnType>(elementRule: RuleFunction<ReturnType>): RuleFunc
       ruleResult = elementRule(state)
     }
     return ok(result)
-  }
+  })
 }
 
 const identiferToken = token('identifier')
 const arrayToken = token('[]')
 const optionalToken = token('?')
 
-function keyword(keywordName: string): RuleFunction<Token> {
-  return defineRule((state) => {
+const keyword = (keywordName: string): RuleFunction<Token> => {
+  return defineRule(() => (state) => {
     return identiferToken(state)
       .orElse((error) => state.error(`Expected keyword ${keywordName}`, error.position))
       .flatMap((token) => {
@@ -266,7 +285,7 @@ function keyword(keywordName: string): RuleFunction<Token> {
 }
 
 function token(tokenType: TokenType): RuleFunction<Token> {
-  return defineRule<Token>((state) => {
+  return defineRule(() => (state) => {
     const token = state.advance()
     if (token.type === tokenType) {
       return ok(token)
@@ -276,11 +295,12 @@ function token(tokenType: TokenType): RuleFunction<Token> {
 }
 
 function defineNodeRule<T, NodeType extends Node>(
-  ruleFn: RuleFunction<T>,
+  ruleFactory: () => RuleFunction<T>,
   toNodeFn: (result: T) => NodeType,
 ): RuleFunction<NodeType> {
-  return defineRule((state) => {
-    return ruleFn(state).map(toNodeFn)
+  return defineRule(() => {
+    const ruleFn = ruleFactory()
+    return (state) => ruleFn(state).map(toNodeFn)
   })
 }
 
@@ -290,12 +310,16 @@ function changeErrorMessage<T>(ruleFn: RuleFunction<T>, message: string): RuleFu
   }
 }
 
-function defineRule<ReturnType>(ruleFn: RuleFunction<ReturnType>): RuleFunction<ReturnType> {
-  return (tokenList) => {
-    const position = tokenList.getPointer()
-    const result = ruleFn(tokenList)
+function defineRule<ReturnType>(ruleFactory: () => RuleFunction<ReturnType>): RuleFunction<ReturnType> {
+  let ruleFn: RuleFunction<ReturnType> | null = null
+  return (state) => {
+    if (!ruleFn) {
+      ruleFn = ruleFactory()
+    }
+    const position = state.getPointer()
+    const result = ruleFn(state)
     if (result.isError()) {
-      tokenList.restorePointer(position)
+      state.restorePointer(position)
     }
     return result
   }
